fc04d10a244b012cd8c1366ed3e947bfa465117f
Bryan Fichera <bfichera@mit.edu>
2020-06-26 14:14:17 -0400

    Fix bug in error reporting

--- arxivscraper.py
+++ arxivscraper.py
@@ -1,32 +1,61 @@
-import arxiv
 from datetime import datetime
+from datetime import timedelta
 import logging
 import argparse
-from dateutil.parser import parse
 import requests
 import io
 import re
-import json
 import smtplib
 import ssl
+from functools import reduce
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+import arxiv
 import slate3k as slate
+from dateutil.parser import parse
+from timeout_decorator import (
+    timeout,
+    TimeoutError,
+)
+    
 
 _logger = logging.getLogger(__name__)
 
 
-def send_email(cfg, matches):
-    message = ''
-    message += 'From: '+cfg['from_email']+'\n'
-    message += 'To: '+cfg['to_email']+'\n'
-    message += 'Subject: Important arxiv post(s)\n\n'
+def send_email(cfg, matches, errors):
+    # Create the message 
+    message = MIMEMultipart('alternative')
+    message['From'] = cfg['from_email']
+    message['To'] = cfg['to_email']
+    message['Subject'] = 'Important arxiv post(s)'
+    text = ''
+    html = '<html>\n'
+    html += '<body>\n'
     for regex, ids in matches.items():
         if ids:
-            message += f'Matches for regex {regex}\n'
+            text += f'Matches for regex {regex}\n'
+            html += f'<p>\nMatches for regex {regex}\n<ul>\n'
             for id_ in ids:
-                message += '    '
-                message += str(id_)
-                message += '\n'
+                text += f'    {str(id_)}\n'
+                html += f'<li><a href="{str(id_)}">{str(id_)}</a></li>\n'
+            html += '</ul>\n</p>\n'
+    text += f'There were {len(errors)} errors.\n'
+    html += f'There were {len(errors)} errors.<br>\n'
+    for error in errors:
+        text += error
+        text += '\n'
+        html += error+'<br>'
+    html += '</body>'
+    html += '</html>'
+
+    part1 = MIMEText(text, 'plain')
+    part2 = MIMEText(html, 'html')
+
+    message.attach(part1)
+    message.attach(part2)
     
+    # Actually send the email
     context = ssl.create_default_context()
 
     with smtplib.SMTP_SSL(
@@ -41,7 +70,7 @@ def send_email(cfg, matches):
         server.sendmail(
             cfg['from_email'],
             cfg['to_email'],
-            message,
+            message.as_string(),
         )
             
 
@@ -58,17 +87,6 @@ def get_pdf_text(url):
     return str(text)
 
 
-def in_pdf(url, list_of_regex, flags=0):
-    text = get_pdf_text(url)
-    answers = {regex:False for regex in list_of_regex}
-    for regex in list_of_regex:
-        prog = re.compile(regex, flags)
-        result = prog.search(text)
-        if result is not None:
-            answers[regex] = True
-    return answers
-
-
 def sanitize_chem_term(chem_term):
     legal = r'(\s|\$|_|\{|\})*'
     chem_list = chem_term.split(' ')
@@ -83,7 +101,6 @@ def sanitize_chem_term(chem_term):
             
 
 def main(cfg):
-
     ##
     ## Make terms list
     ##
@@ -104,7 +121,7 @@ def main(cfg):
     )
     today_results = (
         p for p in results() 
-        if posted_today(p, datetime(2020, 6, 19))
+        if posted_today(p, datetime.today()-timedelta(days=1))
     )
 
     matches = {}
@@ -117,13 +134,26 @@ def main(cfg):
         'VERBOSE': re.VERBOSE,
     }
     flags = [flagsdict[flag.upper()] for flag in cfg['flags']]
+
+    @timeout(cfg['timeout'])
+    def in_pdf(url, list_of_regex, flags=0):
+        text = get_pdf_text(url)
+        answers = {regex:False for regex in list_of_regex}
+        for regex in list_of_regex:
+            prog = re.compile(regex, flags)
+            result = prog.search(text)
+            if result is not None:
+                answers[regex] = True
+        return answers
+
+    errors = []
     for paper in today_results:
         try:
-            logging.info(f'Checking {paper.id}...')
+            _logger.info(f'Checking {paper.id}...')
             for regex, is_in in in_pdf(
                 paper.pdf_url,
                 terms,
-                flags=flags,
+                flags=reduce(int.__or__, flags),
             ).items():
                 if is_in:
                     if regex not in matches.keys():
@@ -131,10 +161,21 @@ def main(cfg):
                     else:
                         matches[regex].append(paper.id)
                     
-        except:
-            pass
+        except Exception as exception:
+            if isinstance(exception, TimeoutError):
+                msg = f'PDF reader timeout reached for {paper.id}.'
+                _logger.warning(msg)
+            else:
+                msg = str(exception)+f' called for {paper.id}.'
+            errors.append(msg)
+                
+    if matches:
+        send_email(cfg, matches, errors)
 
-    send_email(cfg, matches)
+    if errors:
+        _logger.warning('There were PDF errors.')
+        for error in errors:
+            _logger.warning(error)
 
 
 def get_cfg():
